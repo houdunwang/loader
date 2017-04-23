07f0e54f453a32f3c6a3359ed7ee01b00e584b27
houdunwang <2300071698@qq.com>
2017-04-23 19:55:40 +0800

    no message

--- src/LoaderProvider.php
+++ src/LoaderProvider.php
@@ -11,17 +11,22 @@
 
 use houdunwang\framework\build\Provider;
 
-class LoaderProvider extends Provider {
-	//延迟加载
-	public $defer = true;
+class LoaderProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	public function boot() {
+    public function boot()
+    {
+    }
 
-	}
-
-	public function register() {
-		$this->app->single( 'Loader', function () {
-			return new Loader();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'Loader',
+            function () {
+                return new Loader();
+            }
+        );
+    }
 }
